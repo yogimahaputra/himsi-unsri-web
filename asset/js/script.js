f989445b44dd2dc208582032dfbbc52a08d39aee
yogimahaputra <yogi.mahaputra123@gmail.com>
2021-05-06 07:52:00 +0700

    Update

--- asset/js/script.js
+++ asset/js/script.js
@@ -60,3 +60,31 @@ var flkty_three = new Flickity(elem_three, {
     pageDots: true,
     lazyLoad: 1
 });
+
+var message = "HIMSI FASILKOM UNIVERSITAS  SRIWIJAYA 2021";
+
+function rtclickcheck(keyp){ if (navigator.appName == "Netscape" && keyp.which == 3){ alert(message); return false; }
+
+if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) { alert(message); return false; } }
+
+document.onmousedown = rtclickcheck;
+
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+  
+}
